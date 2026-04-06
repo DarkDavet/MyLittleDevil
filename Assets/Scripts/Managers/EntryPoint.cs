6237514bc12483c5934401304c56bed3a0dcedd4
DarkDavet <darkdavet@gmail.com>
2026-04-06 18:28:23 +0300

    EntryPoint - clean unused code

--- Assets/Scripts/Managers/EntryPoint.cs
+++ Assets/Scripts/Managers/EntryPoint.cs
@@ -4,16 +4,10 @@ using UnityEngine;
 
 public class EntryPoint : MonoBehaviour
 {
-   // [SerializeField] private DialoguesStorage dlgStorage;
-   // [SerializeField] private DialogueSystem dlgSystem;
-   // [SerializeField] private DialogueContainerUI dlgUI;
     [SerializeField] private GameStateContext gameStateManager;
 
     private void Start()
     {
-        /*dlgSystem.Init(dlgStorage);
-        dlgUI.Init();
-        dlgSystem.StartDialogue("dlg_1");*/
         gameStateManager.Init();
     }
 }
