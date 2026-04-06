38c130588faa4428875a7d60f87e60c88e5253a9
DarkDavet <darkdavet@gmail.com>
2026-04-06 18:28:58 +0300

    updated attachments in all playable scenes

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -32,11 +32,6 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        
-    }
-
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
