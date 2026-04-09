8d9886e806d3067880934219c9d8d54d7a1a9221
DarkDavet <darkdavet@gmail.com>
2026-04-09 18:38:04 +0300

    Tutorial system - added test implementation

--- Assets/Scripts/Managers/EntryPoint.cs
+++ Assets/Scripts/Managers/EntryPoint.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class EntryPoint : MonoBehaviour
 {
     [SerializeField] private GameStateContext gameStateManager;
+    [SerializeField] private TutorialSystem tutorialSystem;
+    [SerializeField] private TutorialStorage tutorialStorage;
 
     private void Start()
     {
         gameStateManager.Init();
+        tutorialSystem.Init(tutorialStorage);
+        tutorialSystem.StartTutorial("tut_1");
     }
 }
