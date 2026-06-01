cef8ee1f1954a72e2b38bbcaca15da1a4c7806f2
DarkDavet <darkdavet@gmail.com>
2026-06-01 16:05:20 +0300

    added CreditsController

---
+++ Assets/Scripts/Other/CreditsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour
+{
+    [SerializeField] private SceneLoader sceneLoader;
+
+    [SerializeField] private float startDelay = 2f;
+    [SerializeField] private float endDelay = 2f;
+
+    [SerializeField] private float speed = 100f; 
+    [SerializeField] private float endPositionY = 1750f;
+
+    private void Start()
+    {
+        StartCoroutine(CreditsSequence());
+    }
+
+    private IEnumerator CreditsSequence()
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        while (transform.localPosition.y < endPositionY)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(endDelay);
+
+        sceneLoader.ReturnToMainMenu();
+    }
+}
