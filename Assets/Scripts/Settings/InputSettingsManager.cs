3e13d4bc949c4cccba247725ace89ffe459b7b5b
DarkDavet <darkdavet@gmail.com>
2026-06-21 18:06:26 +0300

    started to write game settings' logic

---
+++ Assets/Scripts/Settings/InputSettingsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputSettingsManager : MonoBehaviour
+{
+    public InputActionAsset inputActions;
+
+    private const string SaveKey = "CustomControlBindings";
+
+    private void Awake()
+    {
+        LoadBindings();
+    }
+
+    public void SaveBindings()
+    {
+        string json = inputActions.SaveBindingOverridesAsJson();
+        PlayerPrefs.SetString(SaveKey, json);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadBindings()
+    {
+        if (PlayerPrefs.HasKey(SaveKey))
+        {
+            string json = PlayerPrefs.GetString(SaveKey);
+            inputActions.LoadBindingOverridesFromJson(json);
+        }
+    }
+
+    public void ResetAllBindings()
+    {
+        foreach (var map in inputActions.actionMaps)
+        {
+            map.RemoveAllBindingOverrides();
+        }
+        PlayerPrefs.DeleteKey(SaveKey);
+    }
+}
---
+++ Assets/Scripts/Settings/SettingsManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager Instance;
+
+    [Header("Audio")]
+    public AudioMixer audioMixer;
+
+    [Header("Controls")]
+    public InputSettingsManager inpotSettings;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            LoadSettings();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // --- ГРАФИКА ---
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+    }
+
+    public void SetVSync(bool isVSync)
+    {
+        QualitySettings.vSyncCount = isVSync ? 1 : 0;
+        PlayerPrefs.SetInt("VSync", isVSync ? 1 : 0);
+    }
+
+    // --- АУДИО ---
+    public void SetVolume(string parameterName, float value)
+    {
+        // Переводим значение слайдера (0..1) в децибелы (-80..0)
+        float dB = value > 0 ? Mathf.Log10(value) * 20 : -80f;
+        audioMixer.SetFloat(parameterName, dB);
+        PlayerPrefs.SetFloat(parameterName, value);
+    }
+
+    // --- ЗАГРУЗКА ---
+    private void LoadSettings()
+    {
+        // Графика
+        bool isFullscreen = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
+        Screen.fullScreen = isFullscreen;
+
+        bool isVSync = PlayerPrefs.GetInt("VSync", 1) == 1;
+        QualitySettings.vSyncCount = isVSync ? 1 : 0;
+
+        // Аудио (Громкость по умолчанию: 0.75f)
+        float master = PlayerPrefs.GetFloat("MasterVol", 0.75f);
+        SetVolume("MasterVol", master);
+
+        float music = PlayerPrefs.GetFloat("MusicVol", 0.75f);
+        SetVolume("MusicVol", music);
+    }
+}
