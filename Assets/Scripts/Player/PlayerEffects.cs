3c7bb727105b738fa792f879c46047a88794d635
DarkDavet <darkdavet@gmail.com>
2026-09-17 17:36:22 +0300

    create PlayerEffects, add a related sprite, update player prefab

---
+++ Assets/Scripts/Player/PlayerEffects.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerEffects : MonoBehaviour
+    {
+
+        [Header("Shield Visuals")]
+        [SerializeField] private GameObject shieldVisualObject; // Спрайт/эффект щита вокруг героя
+
+        private Animator animator;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+
+            // Если объект щита передан, изначально выключаем его
+            if (shieldVisualObject != null) shieldVisualObject.SetActive(false);
+        }
+
+        // Включаем или выключаем визуальный щит
+        public void SetShieldVisual(bool isActive)
+        {
+            if (shieldVisualObject != null)
+            {
+                shieldVisualObject.SetActive(isActive);
+            }
+        }
+
+        // Проигрываем анимацию получения урона
+        public void PlayHitAnimation()
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger("Hit");
+            }
+        }
+    }
+}
